0f2977bc559ee8707c9a21f37cc7e4620c445dcd
Cynthia <cynthia2048@proton.me>
2026-02-02 12:19:54 +0530

    First working prototype of an HTTP-aware Zip reader
    zipfile assumed its IO is stateful, however HTTP is a stateless protocol
    so it integrates poorly, and inevitably the performance is absymal.
    
    It's particularly inefficient because amount of bytes to be read is certain,
    and compressed file data is read in a sequence; so instead of broken down
    range requests, a single range request is made (coalescing extents of all
    possible reads) thus being orders of magnitude faster.

--- zipinspect/__main__.py
+++ zipinspect/__main__.py
@@ -1,11 +1,10 @@
+import os.path
 import sys
 import time
 
-from zipfile import ZipFile, ZipInfo
-
 from tabulate import tabulate
 
-from .http import HTTPRandom
+from .zipread import HTTPZipReader, ZipEntryInfo
 from .pages import PaginatedCollection
 from .repl import parse_args
 
@@ -19,9 +18,24 @@ def numfmt_iec(n):
             return f'{n:3.1f}{u}'
         n /= 1024.0
 
+class UnsafePathError(Exception):
+    def __init__(self, path):
+        self.path = path
+        super().__init__()
+
+def sanitized_open(path, **kwargs):
+    path = os.path.abspath(path)
+
+    if os.path.commonpath((os.getcwd(), os.getcwd())) != os.getcwd():
+        raise UnsafePathError("Path is outside the scope of working directory")
+
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    return open(path, **kwargs)
+
+# TODO Use click, and not this.
 if __name__ == "__main__":
-    with (HTTPRandom(sys.argv[1]) as handle, ZipFile(handle, 'r') as z):
-        infos = PaginatedCollection(z.infolist())
+    with HTTPZipReader(sys.argv[1]) as z:
+        infos = PaginatedCollection(z.entries)
 
         while True:
             try:
@@ -34,11 +48,11 @@ if __name__ == "__main__":
 
             match args[0]:
                 case 'list':
-                    def zipinfo_to_row(info: ZipInfo):
-                        size = numfmt_iec(info.file_size) if not info.is_dir() else 'N/A'
-                        timestamp = zipinfo_time_to_iso8601(info.date_time)
+                    def zipinfo_to_row(info: ZipEntryInfo):
+                        size = numfmt_iec(info.file_size) if not info.is_dir else 'N/A'
+                        timestamp = zipinfo_time_to_iso8601(info.modified_date)
 
-                        return info.filename, size, timestamp
+                        return info.path, size, timestamp
 
                     page = [(index, *zipinfo_to_row(info))
                             for index, info in enumerate(infos.current(),
@@ -50,23 +64,31 @@ if __name__ == "__main__":
                 case 'next':
                     infos.next()
                 case 'extract':
+                    # TODO Implement a method of specifying path.
                     if len(args) < 2:
                         print("ERROR: No index provided; retry.")
                         continue
 
-                    ilist = z.infolist()
+                    ilist = z.entries
                     index = int(args[1])
 
                     if 0 <= index <= len(ilist):
                         info = infos.seq[index]
 
-                        if info.is_dir():
-                            for info1 in ilist:
-                                if info1.filename.startswith(info.filename):
-                                    z.extract(info1)
-
-                        else:
-                            z.extract(infos.seq[index])
+                        try:
+                            if info.is_dir:
+                                for info1 in ilist:
+                                    if info1.path.startswith(info.path) and not info1.is_dir:
+                                        with sanitized_open(info1.path, mode='wb') as output:
+                                            z.extract(info1, output)
+                            else:
+                                with sanitized_open(info.path, mode='wb') as output:
+                                    z.extract(info, output)
+                        except UnsafePathError as e:
+                            print(f"Path {e.path} is potentially dangerous, not proceeding.")
                     else:
                         print(f"ERROR: Index out of bounds, max {len(ilist)}")
+
+                case wrong_cmd:
+                    print(f'Not a valid command {wrong_cmd}; try again.')
 
